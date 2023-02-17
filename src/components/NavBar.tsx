802bf6d6104e4031c35dbdf78fc5ad3955f89a9e
theblackman2 <pascal@kinshasadigital.com>
2023-02-17 10:23:12 +0100

    feat: finish navbar design

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { NextRouter, useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const NavBar = () => {
+  const router: NextRouter = useRouter();
+  const [activeLink, setActiveLink] = useState<string>("resume");
+  const [showLinks, setShowLinks] = useState<boolean>(false);
+
+  const toggleLinks = () => {
+    setShowLinks(prev => !prev)
+  }
+
+  useEffect(() => {
+    const currentSection: string = router.asPath.substring(2);
+    setActiveLink(currentSection || "resume")
+    setShowLinks(false);
+  }, [router]);
+
+  return (
+    <div id="nav-bar">
+      <div className="content">
+        <span className="logo">
+          <Link href="#resume">The <span>Black Man</span></Link>
+        </span>
+        <nav className={`links ${showLinks ? "show" : "hide"}`}>
+          <Link className={activeLink == "resume" ? "active" : ""} href="#resume">Resume</Link>
+          <Link className={activeLink == "services" ? "active" : ""} href="#services">Services</Link>
+          <Link className={activeLink == "portfolio" ? "active" : ""} href="#portfolio">Portfolio</Link>
+          <Link className={activeLink == "contact" ? "active" : ""} href="#contact">Contact</Link>
+        </nav>
+      </div>
+      <span onClick={toggleLinks} className="icon">{
+        showLinks ? <AiOutlineClose /> : <AiOutlineMenu />
+      }</span>
+    </div>
+  )
+}
+
+export default NavBar;
